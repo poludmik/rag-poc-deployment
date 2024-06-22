b9f918817e340906699ad0d56ac7ca1641bcbbe9
poludmik <michael.poludin@gmail.com>
2024-06-22 14:01:09 +0200

    Zephyr inference test

--- backend.dockerfile
+++ backend.dockerfile
@@ -10,9 +10,11 @@ WORKDIR /app
 
 COPY requirements_backend.txt /app/requirements_backend.txt
 COPY backend.py /app/backend.py
+COPY download_the_model.py /app/download_the_model.py
 
 # RUN --mount=type=cache,target=/root/.cache/pip pip install -r requirements_backend.txt
 RUN pip install --no-cache-dir --upgrade -r requirements_backend.txt
+RUN python download_the_model.py
 
 EXPOSE 8080
 CMD exec uvicorn --port 8080 --host 0.0.0.0 backend:app
--- backend.py
+++ backend.py
@@ -7,6 +7,37 @@ from pydantic import BaseModel
 # classes = ['class1', 'class2', 'class3']
 # print("Classes:", classes)
 
+from llama_cpp import Llama
+
+
+def generate_text(
+    prompt="Who is the CEO of Apple?",
+    max_tokens=256,
+    temperature=0.1,
+    top_p=0.5,
+    echo=False,
+    stop=["#"],
+):
+    output = llm(
+        prompt,
+        max_tokens=max_tokens,
+        temperature=temperature,
+        top_p=top_p,
+        echo=echo,
+        stop=stop,
+    )
+    output_text = output["choices"][0]["text"].strip()
+    return output_text
+
+
+def generate_prompt_from_template(input):
+    chat_prompt_template = f"""<|im_start|>system
+You are a helpful chatbot.<|im_end|>
+<|im_start|>user
+{input}<|im_end|>"""
+    return chat_prompt_template
+
+
 app = FastAPI()
 
 def get_answer(filename: str, question: str) -> str:
@@ -35,6 +66,14 @@ async def answer(request: QuestionRequest):
         print("question:", request.question)
         answer = get_answer(request.filename, request.question)
         print("answer:", answer)
+
+        llm_answer = generate_text(
+            request.question,
+            max_tokens=356,
+            )
+
+        answer += llm_answer
+        print("answer:", answer)
         return {"filename": request.filename, "question": request.question, "answer": answer}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -45,3 +84,5 @@ async def app_startup():
     global classes
     classes = ['class1', 'class2', 'class3']
     print("Classes initialized:", classes)
+    global llm
+    llm = Llama(model_path="zephyr-7b-beta.Q4_0.gguf", n_ctx=512, n_batch=126)
